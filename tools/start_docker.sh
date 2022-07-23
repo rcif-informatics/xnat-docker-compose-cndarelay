b054fec42c75948bddfa3a7968c7f09e0f32412d
Michael Hodge <hodgem@wustl.edu>
2022-07-22 23:03:07 -0500

    Add restart functionality

--- tools/start_docker.sh
+++ tools/start_docker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+ 
+if [[ "$*" =~ ^.*\-\-restart ]] ; then
+        docker-compose -f /data/docker/xnat-docker-compose-ccfrelay/docker-compose.yml down
+fi
 
 if [ `docker ps -f name=xnat -q | wc -l` -lt 3 ] ; then
 	docker-compose -f /data/docker/xnat-docker-compose-ccfrelay/docker-compose.yml up -d
