e64b99d2a08b9d02d259d29842959f888d81c480
Michael Hodge <hodgem@wustl.edu>
2022-08-12 07:40:16 -0500

    Add script to set XSync site preferences

---
+++ tools/set_xsync_site_preferences.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -n -X POST "https://localhost/xapi/xsyncSitePreferences" -H "accept: */*" -H "Content-Type: text/plain" -d "{ \"tokenRefreshInterval\": \"10 hours\", \"syncRetryCount\": \"2\", \"syncRetryInterval\": \"2 hours\"}"
+
+curl -n -X POST "https://localhost/xapi/xsyncSitePreferences/aspera" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"asperaNodeUrl\": \"asp-connect1.wustl.edu\", \"asperaNodeUser\": \"xnat\", \"privateKey\": \"/data/xnat/home/aspera_keys/ccfrelay-ecdsa-key\", \"destinationDirectory\": \"/data/intradb/inbox/xar/\", \"logDirectory\": \"/tmp/\", \"sshPort\": \"33001\", \"udpPort\": \"33001\"}"
+
+curl -n -X POST "https://localhost/xapi/xsyncSitePreferences/cliTransfer" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"cliTransferScript\": \"/data/xnat/home/relay_tools/xsync_transfer_file\", \"cliTransferHost\": \"asp-connect1.wustl.edu\", \"cliTransferUser\": \"relay-cli\", \"cliTransferRemoteDir\": \"/data/intradb/inbox/xar\", \"cliTransferPrivateKey\": \"/data/xnat/home/aspera_keys/ccfrelay-ecdsa-key\"}"
+
